4ac3adef5cd10d3de5f9bb84ee18a646b0fe2f5f
ryoshimura <snd.hp0015@gmail.com>
2026-03-26 18:05:26 +0900

    commit

--- app/next.config.mjs
+++ app/next.config.mjs
@@ -1,7 +1,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   // Docker環境(dev/stg)はstandalone、Vercel環境はundefined
-  ...(process.env.DEPLOY_TARGET === "docker" && { output: "standalone" }),
+  ...(process.env.DEPLOY_TARGET === "docker"),
   experimental: {
     serverComponentsExternalPackages: ["@prisma/client"],
   },
